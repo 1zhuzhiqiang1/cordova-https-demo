992e92f246bfcd042622f577bb2fe605da23e0ed
zhuzhiqiang <mzhuzhiqiang@163.com>
2017-07-24 09:53:04 +0800

    zhuzhiqiang

--- platforms/ios/www/plugins/cordova-plugin-clientcertificate/www/clientCertificate.js
+++
@@ -1,9 +0,0 @@
-cordova.define("cordova-plugin-clientcertificate.clientCertificate", function(require, exports, module) {
-/*global cordova, module*/
-
-module.exports = {
-    register: function (certificatePath, certificatePassword, successCallback, errorCallback) {
-        cordova.exec(successCallback, errorCallback, "ClientCertificate", "register", [certificatePath, certificatePassword]);
-    }
-};
-});
